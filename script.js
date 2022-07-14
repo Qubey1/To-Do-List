d5f846b7e73bd6269dddbc961b98c6d44b18f901
Qubey1 <108349389+Qubey1@users.noreply.github.com>
2022-07-14 17:14:43 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,90 +1,42 @@
 let taskList = JSON.parse(localStorage.getItem("tasks")) || [];
-let addTask = document.getElementById("add-task-button");
+const list = document.getElementById('task-list');
+const task = document.getElementById('input-task');
 let check = document.getElementsByTagName("checkbox");
 
-function delTask (task) {
-    task.parentNode.remove();
-    rebuildTask();
+displayTasks();
+
+function deleteItem(index) {
+    taskList.splice(index, 1);
+    localStorage.setItem('tasks', JSON.stringify(taskList))
+    displayTasks();
 }
 
-buildTask();
+function addItem() {
+    taskList.push({
+        value: task.value,
+        checked: false,
+    })
 
-function buildTask() {
-    let num = taskList.length / 2;
-    for (let i = 0; i < num; i++) {
-        addingTask(taskList[2 * i], taskList[2 * i + 1]);
-    }
+    localStorage.setItem('tasks', JSON.stringify(taskList));
+    task.value='';
+    displayTasks();
 }
 
-function rebuildTask () {
-    let listChildren = document.querySelectorAll(".task");
-    let numb = listChildren.length;
-    let listArray = new Array(numb);
-    for (let i = 0; i < numb; i++) {
-        listArray[2 * i] = listChildren[i].innerHTML;
-        listArray[2 * i + 1] = listChildren[i].previousSibling.checked;
-    }
-    console.log(listArray);
-    localStorage.setItem("tasks", JSON.stringify(listArray));
+function displayTasks(){
+    let items = ""
+    taskList.forEach(function(task) {
+        items +=`<li><input type="checkbox"><span class="task">${task.value}</span>
+        <button class="delete-btn" onclick="deleteItem();">X</button></li>`
+    })
+    list.innerHTML = items;
 }
-
-addTask.addEventListener("mouseover", function () {
-    addTask.style.color = "black";
-    addTask.style.background = "white";
-    addTask.style.border = "1px solid black"
-})
-addTask.addEventListener("mouseout", function () {
-    addTask.style.color = "";
-    addTask.style.background = "";
-    addTask.style.border = "";
-})
-
-addTask.addEventListener("click", function () {
-    addingTask();
-    rebuildTask();
-})
-
-function addingTask () {
-    let text = document.querySelector("#input-task").value
-    let ul = document.getElementById("task-list");
-    const li = document.createElement("li");
-    const div = document.createElement("div");
-    const checkBox = document.createElement("input");
-    checkBox.setAttribute("type", "checkbox");
-/*    checkBox.addEventListener("change", e => {
-        check(checkBox);
-    })*/
-    const span = document.createElement("span");
-    span.setAttribute("class", "task");
-    span.appendChild(document.createTextNode(text));
-    div.appendChild(checkBox);
-    div.appendChild(span);
-    const delBtn = document.createElement("button");
-    delBtn.setAttribute("class", "delete-btn");
-    delBtn.setAttribute("onclick", "delTask(this)");
-    li.appendChild(div);
-    li.appendChild(delBtn);
-    ul.appendChild(li);
-    document.getElementById("input-task").value = "";
-}
-
-/*function check(checkBox) {
-    let span = document.querySelector("#task");
-    if (checkBox.checked) {
-        span.classList.add('crossedOut');
-    } else {
-        span.classList.remove('crossedOut');
-    }
-}*/
 
 check.addEventListener("change", function () {
     let checkBox = document.getElementById("checkBox");
     let span = document.getElementById("task")
     if (checkBox.checked) {
-        // alert("выбрано");
         span.classList.add("crossedOut");
     } else {
-        // alert("не выбрано");
         span.classList.remove("crossedOut");
     }
 })
